3e8850ca54c1618dba73a4739a86b8dfb99de47e
Robert Friel <robert@98point6.com>
2022-10-05 16:38:29 -0700

    fix for newer pl version

--- main.py
+++ main.py
@@ -297,7 +297,7 @@ class ImageLogger(Callback):
         self.batch_freq = batch_frequency
         self.max_images = max_images
         self.logger_log_images = {
-            pl.loggers.TestTubeLogger: self._testtube,
+            # pl.loggers.TestTubeLogger: self._testtube,
         }
         self.log_steps = [2 ** n for n in range(int(np.log2(self.batch_freq)) + 1)]
         if not increase_log_steps:
